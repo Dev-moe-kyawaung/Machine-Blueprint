06d3a88d4207c20209c07ac0f3407371cec005c4
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-09-14 19:21:00 +0630

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+    ],
+  },
+  experimental: {
+    optimizePackageImports: ['lucide-react', '@react-three/drei'],
+  },
+};
+
+module.exports = nextConfig;
